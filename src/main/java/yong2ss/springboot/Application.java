896a72fff38313656629c28ff214ec748b26fcfc
jeongyolee <jeongyolee@ebay.com>
2023-02-11 22:03:40 +0900

    독립 실행형 서블릿 어플리케이션 with Servlet (without Spring)

--- src/main/java/yong2ss/springboot/Application.java
+++ src/main/java/yong2ss/springboot/Application.java
@@ -1,13 +1,53 @@
 package yong2ss.springboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-@SpringBootApplication
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            HelloController helloController = new HelloController();
+            servletContext.addServlet("frontcontroller", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    // 인증, 보안, 다국어, 공통 기능 등등
+                    // ...
+
+                    // 매핑
+                    if(req.getRequestURI().equals("/hello")
+                            && req.getMethod().equals(HttpMethod.GET.name())) {
+                        String name = req.getParameter("name");
+
+                        String result = helloController.hello(name);
+
+                        resp.setStatus(HttpStatus.OK.value());
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().println("Hello " + result);
+                    } else if (req.getRequestURI().equals("/user")) {
+                        // ...
+                    } else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
+
+                }
+            }).addMapping("/*");
+        });
+        webServer.start();
     }
 
 }
--- src/main/java/yong2ss/springboot/HelloController.java
+++ src/main/java/yong2ss/springboot/HelloController.java
@@ -1,9 +1,7 @@
 package yong2ss.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 public class HelloController {
     @GetMapping("/hello")
     public String hello(String name) {
