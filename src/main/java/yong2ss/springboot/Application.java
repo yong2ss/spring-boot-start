7a7ddaf115ae1273317c248e39fdcbbaa325210b
jeongyolee <jeongyolee@ebay.com>
2023-02-12 22:05:05 +0900

    Dispatcher Servlet 사용

--- src/main/java/yong2ss/springboot/Application.java
+++ src/main/java/yong2ss/springboot/Application.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -20,37 +22,16 @@ import java.io.IOException;
 public class Application {
 
     public static void main(String[] args) {
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
 
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("frontcontroller", new HttpServlet() {
-                @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    // 인증, 보안, 다국어, 공통 기능 등등
-                    // ...
-
-                    // 매핑
-                    if(req.getRequestURI().equals("/hello")
-                            && req.getMethod().equals(HttpMethod.GET.name())) {
-                        String name = req.getParameter("name");
-
-                        HelloController helloController = applicationContext.getBean(HelloController.class);
-                        String result = helloController.hello(name);
-
-                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println(result);
-                    } else if (req.getRequestURI().equals("/user")) {
-                        // ...
-                    } else {
-                        resp.setStatus(HttpStatus.NOT_FOUND.value());
-                    }
-
-                }
-            }).addMapping("/*");
+            servletContext.addServlet("dispatcherServlet",
+                    new DispatcherServlet(applicationContext)
+            ).addMapping("/*");
         });
         webServer.start();
     }
--- src/main/java/yong2ss/springboot/HelloController.java
+++ src/main/java/yong2ss/springboot/HelloController.java
@@ -1,9 +1,12 @@
 package yong2ss.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Objects;
 
+@RequestMapping
 public class HelloController {
     private final HelloService helloService;
 
@@ -12,6 +15,7 @@ public class HelloController {
     }
 
     @GetMapping("/hello")
+    @ResponseBody //없이 String return하면 dispatcherServlet이 view page를 찾음
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
